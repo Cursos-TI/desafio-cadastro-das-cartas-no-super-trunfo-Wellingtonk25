f06a8b4599f6fb7422aaf8dbc27251ad14fc6f66
Wellingtonk25 <116383373+Wellingtonk25@users.noreply.github.com>
2025-04-03 05:43:42 +0000

    Cadastro das cartas

---
+++ CadastrocartasSuperTrunfo.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main(){
+    //Aqui estamos declarando as variáveis que iremos utilizar na aplicação.
+    char idcarta[50];
+    char estado[30];
+    char cidade[50];
+    int populacao, turismo;
+    float areakm;
+    float pib;
+
+
+    //Aqui estamos pedindo ao usuário que informe todos os dados solicitados.
+    printf ("Digite o ID da carta: \n");
+    scanf ("%s", &idcarta);
+
+    printf ("Digite o Estado: \n");
+    scanf ("%s", &estado);
+
+    printf ("Digite o nome da cidade: \n");
+    scanf ("%s", &cidade);
+
+    printf ("Digite a população: \n");
+    scanf ("%d", &populacao);
+
+    printf ("Digite a Área em Km²: \n");
+    scanf ("%f", &areakm);
+
+    printf ("Digite o PIB da Estado: \n");
+    scanf ("%f", &pib);
+
+    printf ("Digite a quantidade de pontos turisticos: \n");
+    scanf ("%d", &turismo);
+
+    //Aqui estamos exibindo o resumo juntamente com os dados informados pelo usuário.
+    printf("Código da carta: %s\n", idcarta);
+    printf("Estado: %s\n", estado);
+    printf("Nome da Cidade: %s\n", cidade);
+    printf("População: %d\n", populacao);
+    printf("Área em Km²: %f\n", areakm);
+    printf("PIB: %f\n", pib);
+    printf("Número de pontos turísticos: %d", turismo);
+
+
+    return 0;
+
+
+}
